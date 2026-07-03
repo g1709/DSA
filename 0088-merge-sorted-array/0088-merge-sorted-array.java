f94929bbe75acd170c432e47981a532c681447d7
g1709 <gs2442616@gmail.com>
2026-07-03 22:13:48 +0530

    Time: 1 ms (25.58%), Space: 43.6 MB (76.3%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<m; i++){
+            list.add(nums1[i]);
+        }
+        for(int v : nums2){
+            list.add(v);
+
+        }
+        Collections.sort(list);
+        for(int i=0; i<list.size(); i++){
+            nums1[i] = list.get(i);
+        }
+        
+    }
+}
