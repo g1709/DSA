a029353733160008cb63817c140f1c70c46916fe
g1709 <gs2442616@gmail.com>
2026-07-04 10:12:32 +0530

    Time: 3 ms (96.26%), Space: 52.2 MB (54.31%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,28 +1,22 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        int n = nums.length; 
         int i=0;
         int j=0;
-       int zero=0;
+        int zero=0;
         int maxi=0;
-        int n = nums.length;
-        
         while(i<n){
-          if(nums[i]==0){
-            zero++;
-          }
+            if(nums[i]==0) zero++;
             while(zero>k){
                 if(nums[j]==0){
                     zero--;
-                   
-                    
                 }
                 j++;
             }
-             i++;
-            maxi=Math.max(maxi, i-j);
-           
-
+            maxi = Math.max(maxi, i-j+1);
+           i++;
         }
         return maxi;
+        
     }
 }
