36f7c1ceb5042564b10e9dc83c15ee6e9edf6aca
g1709 <gs2442616@gmail.com>
2026-07-07 19:54:43 +0530

    Time: 8 ms (1.3%), Space: 46.4 MB (55.13%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,48 +10,39 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         ListNode temp = head;
         while(temp!=null){
-            answer.add(temp.val);
+            ans.add(temp.val);
             temp=temp.next;
         }
-        int n = answer.size();
+
         int i=0;
         int j=0;
-        ArrayList<Integer> result = new ArrayList<>();
-        while(i<n){
-            st.push(answer.get(i));
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        while(i<ans.size()){
+            st.push(ans.get(i));
             while(i-j+1==k){
-                
                 while(!st.isEmpty()){
-                    
-                    result.add(st.pop());
+                    answer.add(st.pop());
                 }
-                j=i+1;
-            
+            j=i+1;
             }
-        i++;
+            i++;
         }
-        ArrayList<Integer> faltu = new ArrayList<>();
-        while(!st.isEmpty()){
-            faltu.add(st.remove(0));
-        }
-        while(!st.isEmpty()){
-        result.add(st.remove(0));   
-        }
-        for(int l=0; l<faltu.size(); l++){
-            result.add(faltu.get(l));
+
+        while(j<ans.size()){
+            answer.add(ans.get(j));
+            j++;
         }
         ListNode dummy = new ListNode(0);
         ListNode dummy1 = dummy;
-        for(int ans : result){
-            dummy1.next = new ListNode(ans);
-            dummy1=dummy1.next;
+        for(int v : answer){
+            dummy1.next = new ListNode(v);
+            dummy1 = dummy1.next;
         }
         return dummy.next;
-        
         
     }
 }
