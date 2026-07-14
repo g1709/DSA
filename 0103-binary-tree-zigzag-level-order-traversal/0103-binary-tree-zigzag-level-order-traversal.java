1700546c5969386005352052b936946e3c375e86
g1709 <gs2442616@gmail.com>
2026-07-14 19:37:29 +0530

    Time: 0 ms (100%), Space: 43.7 MB (44.46%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,42 +16,44 @@
 class Solution {
     public static int height(TreeNode root){
         if(root==null) return 0;
-        return 1 + Math.max(height(root.left), height(root.right));
+        return 1+Math.max(height(root.left), height(root.right));
     }
 
-    public static void z1(TreeNode root, int n, ArrayList<Integer> ans){
+
+    public static void solvez1(TreeNode root, ArrayList<Integer> ans, int n){
         if(root==null) return;
         if(n==1){
-            
             ans.add(root.val);
             return;
         }
-        z1(root.left, n-1, ans);
-        z1(root.right, n-1, ans);
+        solvez1(root.left, ans, n-1);
+        solvez1(root.right, ans, n-1);
+
     }
-    public static void z2(TreeNode root, int n, ArrayList<Integer> ans){
+    public static void solvez2(TreeNode root, ArrayList<Integer> ans, int n){
         if(root==null) return;
         if(n==1){
             ans.add(root.val);
             return;
         }
-        z2(root.right, n-1, ans);
-        z2(root.left, n-1, ans);
+        solvez2(root.right, ans, n-1);
+        solvez2(root.left, ans, n-1);
+
     }
 
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        int level = height(root)+1;
         List<List<Integer>> answer = new ArrayList<>();
+        
+        int level = height(root);
         for(int i=1; i<=level; i++){
             ArrayList<Integer> ans = new ArrayList<>();
-            if(i%2==0) z2(root, i, ans);
-            else z1(root, i, ans);
+            if(i%2==0) solvez2(root, ans, i);
+            else solvez1(root, ans, i);
             if(ans.size()>0){
-            answer.add(new ArrayList<>(ans));
+                answer.add(ans);
             }
         }
         return answer;
-        
     }
 }
