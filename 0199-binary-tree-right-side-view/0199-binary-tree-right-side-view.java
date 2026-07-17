1fafa08bfc04848b6654cf154789bf2606565ee5
g1709 <gs2442616@gmail.com>
2026-07-17 19:42:57 +0530

    Time: 1 ms (71.7%), Space: 43.9 MB (14.96%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,25 +14,25 @@
  * }
  */
 class Solution {
-    public static void solve(TreeNode root, ArrayList<Integer> ans, int height){
-        if(root==null) return;
-
-        if(height==ans.size()){
-            ans.add(root.val);
-        }
-
-        solve(root.right, ans, height+1);
-        solve(root.left, ans, height+1);
-
-
-    }
-
-
     public List<Integer> rightSideView(TreeNode root) {
+        if(root==null) return new ArrayList<>();
         ArrayList<Integer> ans = new ArrayList<>();
-        solve(root, ans, 0);
-        return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+       
+        while(!q.isEmpty()){
+             int size = q.size();
+              
+            for(int i=0; i<size; i++){
+               TreeNode temp = q.remove();
+                if(i==size-1){
+                    ans.add(temp.val);
+                }
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
+            }
 
-        
+        }
+    return ans;
     }
 }
