38a561ddb23e068fe338472fe2a39cedb3edf9eb
g1709 <gs2442616@gmail.com>
2026-07-20 20:20:16 +0530

    Time: 0 ms (100%), Space: 61.1 MB (71.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,13 +1,29 @@
+//approach phle 0 se n-1 tk reverse
+//phir 0 se k-1 tk
+//phir k se nums.length-1 tk
 class Solution {
+    public static void reverse(int[] nums, int i, int j){
+
+        
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+
+        }
+
+    }
+
+
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        int arr[] = new int[n];
-        for(int i=0; i<n; i++){
-            arr[(i+k)%n]=nums[i];
-        }
-        for(int i=0; i<n; i++){
-            nums[i]=arr[i];
-        }
+        k=k%n;
+        if(k>n) return;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
         
     }
 }
