1050f71cac246c02a641705fad0b334e78d19201
g1709 <gs2442616@gmail.com>
2026-07-22 17:43:59 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.39%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,14 +1,14 @@
 class Solution {
-    public static int solve(int nums[], int target, int low, int high){
-        int mid = (low+high)/2;
-        if(low>high) return -1;
-        if(nums[mid]>target)   return solve(nums, target, low, mid-1);
-        else if(nums[mid]==target) return mid;
-         return solve(nums, target, mid+1, high);
-
-    }
     public int search(int[] nums, int target) {
-        return solve(nums, target, 0, nums.length-1);
-        
+        int n = nums.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return -1;
     }
 }
