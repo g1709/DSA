f440f60bd206516bec26a72e14fac1103a8fd4d7
g1709 <gs2442616@gmail.com>
2026-07-22 10:10:54 +0530

    Time: 1 ms (99.97%), Space: 42.9 MB (11.38%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while(x!=0){
+            if(ans>Integer.MAX_VALUE/10 || ans<Integer.MIN_VALUE/10) return 0;
+            int temp = x%10;
+            ans = ans*10+temp;
+            x=x/10;
+        }
+        return ans;
+    }
+}
