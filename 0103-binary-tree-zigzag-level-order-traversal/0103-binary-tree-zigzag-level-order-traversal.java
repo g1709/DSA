6fd6c84bfffa32c025ac8d3ec4deab9a01a8050a
g1709 <gs2442616@gmail.com>
2026-07-26 17:56:24 +0530

    Time: 0 ms (100%), Space: 43.8 MB (44.42%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,45 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public static int height(TreeNode root){
         if(root==null) return 0;
         return 1+Math.max(height(root.left), height(root.right));
     }
-
-
-    public static void solvez1(TreeNode root, ArrayList<Integer> ans, int n){
+    public static void one(TreeNode root,  ArrayList<Integer> ans, int n){
         if(root==null) return;
         if(n==1){
             ans.add(root.val);
             return;
         }
-        solvez1(root.left, ans, n-1);
-        solvez1(root.right, ans, n-1);
-
+        one(root.left, ans, n-1);
+        one(root.right, ans, n-1);
     }
-    public static void solvez2(TreeNode root, ArrayList<Integer> ans, int n){
+
+    public static void two(TreeNode root, ArrayList<Integer> ans, int n){
         if(root==null) return;
         if(n==1){
             ans.add(root.val);
             return;
         }
-        solvez2(root.right, ans, n-1);
-        solvez2(root.left, ans, n-1);
-
+        two(root.right, ans, n-1);
+        two(root.left, ans, n-1);
     }
+
 
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
@@ -48,12 +32,13 @@ class Solution {
         int level = height(root);
         for(int i=1; i<=level; i++){
             ArrayList<Integer> ans = new ArrayList<>();
-            if(i%2==0) solvez2(root, ans, i);
-            else solvez1(root, ans, i);
+            if(i%2==0) two(root, ans, i);
+            else one(root, ans, i);
             if(ans.size()>0){
                 answer.add(ans);
             }
         }
-        return answer;
+return answer;
+        
     }
 }
