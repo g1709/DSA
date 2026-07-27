ff479bd6b4d746a8706a0534290b77e08b9ea8d5
g1709 <gs2442616@gmail.com>
2026-07-27 10:23:43 +0530

    Time: 0 ms (100%), Space: 42 MB (52.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,19 @@
 class Solution {
+    static int dp[];
+    public static int solve(int n){
+    if(n<=0) return 0;
+    if(n==1 || n==2) return 1;
+    if(dp[n]!=0) return dp[n];
+    return dp[n] =  solve(n-1)+solve(n-2);
+    }
+
+
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        return fib(n-1)+fib(n-2);
+         dp = new int[n+1];
+         return solve(n);
+
+
+
         
     }
 }
