248f5dd824a5fc4c6a449cb177c6c40d03b19954
g1709 <gs2442616@gmail.com>
2026-07-28 21:12:22 +0530

    Time: 0 ms (100%), Space: 42.6 MB (78.71%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,12 +1,13 @@
 class Solution {
     static int dp[];
-    public static int solve(int i, int end,  int nums[]){
+    public static int solve(int i, int end, int[] nums){
         if(i>end) return 0;
         if(dp[i]!=-1) return dp[i];
         int take = nums[i] + solve(i+2, end, nums);
-        int skip = solve(i+1, end,  nums);
-        return dp[i] =  Math.max(take, skip);
+        int not = solve(i+1, end, nums);
+        return dp[i] =  Math.max(take, not); 
     }
+
 
 
     public int rob(int[] nums) {
@@ -14,12 +15,11 @@ class Solution {
         if(n==1) return nums[0];
          dp = new int[n+1];
         Arrays.fill(dp, -1);
-        int case1 = solve(0, n-2, nums);
-
-        dp = new int[n+1];
+        int one = solve(0, n-2, nums);
+         dp = new int[n+1];
         Arrays.fill(dp, -1);
-        int case2 = solve(1, n-1, nums);
-        return Math.max(case1, case2);
+        int two = solve(1, n-1, nums);
+        return Math.max(one, two);
         
     }
 }
