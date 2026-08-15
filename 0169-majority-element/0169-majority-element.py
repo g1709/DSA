b440bce05c8657234e19174fc8cd725584073ff4
g1709 <gs2442616@gmail.com>
2026-08-15 22:20:40 +0530

    Time: 17 ms (16.16%), Space: 13.6 MB (73.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        n = len(nums)
         freq = {}
         for num in nums:
             freq[num] = freq.get(num, 0)+1
-        
-        for numbers in nums:
-            temp = freq.get(numbers)
-            if temp > len(nums)/2:
-               return numbers
+        for num, count in freq.items():
+            if count > n//2:
+                return num
         return -1
