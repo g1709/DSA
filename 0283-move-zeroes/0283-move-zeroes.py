81ee3ab3d47e8acf70c3807360c4abd8d832f77d
g1709 <gs2442616@gmail.com>
2026-08-15 10:24:47 +0530

    Time: 13 ms (16%), Space: 13.3 MB (88.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,12 +7,10 @@ class Solution(object):
         i = 0
         j = 0
         while i < len(nums):
-          if nums[i] != 0:
-             temp = nums[i]
-             nums[i] = nums[j]
-             nums[j] = temp
-             j+=1
-          i+=1
-       
-        
+            if nums[i] != 0:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                j+=1
+            i+=1
         
