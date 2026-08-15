cb5e07cb571a53e6780ddd6442cc67b9141fe753
g1709 <gs2442616@gmail.com>
2026-08-15 22:23:56 +0530

    Time: 51 ms (84.65%), Space: 21.1 MB (69.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        sum=0
-        maxi=nums[0]
+        ans = 0
+        maxi = nums[0]
         for i in range(len(nums)):
-            sum+=nums[i]
-            maxi=max(maxi, sum)
-            if sum<0:
-                sum=0
+            ans += nums[i]
+            maxi = max(maxi, ans)
+            if ans < 0:
+                ans = 0
         return maxi
+
         
