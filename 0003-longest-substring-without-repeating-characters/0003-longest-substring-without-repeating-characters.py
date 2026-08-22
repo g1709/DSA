bcbc7b12bb8c1f35f59510f211300aad6b921628
g1709 <gs2442616@gmail.com>
2026-08-22 22:24:51 +0530

    Time: 379 ms (30.25%), Space: 13.4 MB (40.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,17 +5,15 @@ class Solution(object):
         :rtype: int
         """
         seen = set()
-        i=0
         j=0
-        count=0
         maxi=0
-        for i, ch in enumerate(s):
+
+        for i,ch in enumerate(s):
             while ch in seen:
                 seen.remove(s[j])
                 j+=1
             seen.add(ch)
-            
-            maxi = max(maxi, i-j+1)
-        return maxi
-            
+            maxi=max(maxi, i-j+1)
         
+        return maxi
+
