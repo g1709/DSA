a326f4032bc2218c0a192932f9ea3d1683de5120
g1709 <gs2442616@gmail.com>
2026-08-22 22:28:19 +0530

    Time: 33 ms (43.13%), Space: 13.3 MB (93.78%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxi = 0
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] == 1:
+        count=0
+        maxi=0
+        for num in nums:
+            if num==1:
                 count+=1
-                maxi=max(maxi, count)
+                maxi=max(maxi,count)
             else:
                 count=0
         return maxi
