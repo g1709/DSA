db4f37408b561abeddf25411b156739fd7f2bfac
g1709 <gs2442616@gmail.com>
2026-08-22 12:06:34 +0530

    Time: 21 ms (32.51%), Space: 12.3 MB (87.75%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,21 +4,21 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        INT_MAX = 2**31-1
-        INT_MIN = -2**31
-        flag = False
-        if x < 0:
-            flag = True
-            x = x*-1
-        ans = 0
-        while x != 0:
-            temp = x%10
-            ans = ans*10+temp
-            x = x//10
-        if flag == True:
-            ans = ans * -1
+        value = 0
+        count=0
+        if x<0:
+            count=count+1
+        x=abs(x)
+        while x!=0:
+            temp=x%10
+            value=value*10+temp
+            x=x//10
+        if count==1:
+            value=value*-1
 
-        if ans > INT_MAX or ans < INT_MIN:
-                return 0
-        return ans
+        if value < -2**31 or value > 2**31 - 1:
+            return 0
+        return value
+
+
         
