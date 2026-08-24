91cfed24885900d02e740474831d70e4a8dd2aeb
g1709 <gs2442616@gmail.com>
2026-08-24 15:37:41 +0530

    Time: 9 ms (29.02%), Space: 15.1 MB (5.76%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -5,11 +5,12 @@ class Solution(object):
         :rtype: int
         """
         freq = {}
-        for num in  nums:
+        for num in nums:
             freq[num] = freq.get(num, 0)+1
-
         for key, value in freq.items():
             if value == 1:
                 return key
         return -1
+
+
         
