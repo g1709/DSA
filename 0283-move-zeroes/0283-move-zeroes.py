5965b4e6c599c683d240240067c4a8514fa1063d
g1709 <gs2442616@gmail.com>
2026-08-24 15:28:11 +0530

    Time: 5 ms (67.88%), Space: 13.3 MB (97.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,16 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        j = 0
-        n = len(nums)
-        while i < n:
+        i=0
+        j=0
+        while i < len(nums):
             if nums[i] != 0:
                 temp = nums[i]
                 nums[i] = nums[j]
                 nums[j] = temp
                 j+=1
             i+=1
-
-        
         
