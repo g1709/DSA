bc75e2b52cfd9e70f835fbeb2772bf5009b0eb32
g1709 <gs2442616@gmail.com>
2026-08-31 10:17:21 +0530

    Time: 5 ms (82.17%), Space: 45.4 MB (98.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int  dup=x;
-        if(x<0) return false;
-        int ans=0;
+        boolean flag = false;
+        int ans = 0;
+        int dup = x;
+        if(x<0){
+            flag = true;
+            
+        }
         while(x!=0){
             int temp = x%10;
             ans = ans*10 + temp;
             x=x/10;
+
         }
+
+        if(flag == true){
+        ans = ans * -1;
+            }
+
+        if(ans == dup) return true;
+      
+        return false;
         
-        if(ans!=dup) return false;
-        return true;
     }
 }
