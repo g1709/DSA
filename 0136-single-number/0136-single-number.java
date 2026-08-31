ded14f2acbe940278318c591b57d45b0838ab845
g1709 <gs2442616@gmail.com>
2026-08-31 12:18:49 +0530

    Time: 12 ms (22.69%), Space: 46.9 MB (46.02%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,13 +3,11 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0)+1);
-        }        
-
-        for(int k : nums){
-            if(map.containsKey(k)){
-                if(map.get(k)==1) return k;
-            }
+        }
+        for(int num : nums){
+            if(map.get(num)==1) return num;
         }
         return -1;
+        
     }
 }
