078cf9193fdb4a3ece54c210e1d71a6603dd2d62
g1709 <gs2442616@gmail.com>
2026-09-12 12:29:24 +0530

    Time: 1 ms (81.76%), Space: 43.5 MB (90.05%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -14,44 +14,35 @@
  * }
  */
 class Solution {
-    public static int height(TreeNode root){
-        if(root==null) return 0;
-        return 1+Math.max(height(root.left), height(root.right));
-    }
-
-
-    public static void solve1(TreeNode root, int n, ArrayList<Integer> ans){
-        if(root==null) return;
-        if(n==1){
-            ans.add(root.val);
-            return;
-        }
-
-        solve1(root.left, n-1, ans);
-        solve1(root.right, n-1, ans);
-    }
-
-    public static void solve2(TreeNode root, int n, ArrayList<Integer> ans){
-        if(root==null) return;
-        if(n==1){
-            ans.add(root.val);
-            return;
-        }
-        solve2(root.right, n-1, ans);
-        solve2(root.left, n-1, ans);
-    }
-
-
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> answer = new ArrayList<>();
-        int level = height(root);
-        for(int i=1; i<=level; i++){
+        List<List<Integer>> answer =  new ArrayList<>();
+        if(root==null) return answer;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int level = 0;
+        while(!q.isEmpty()){
             ArrayList<Integer> ans = new ArrayList<>();
-            if(i%2==0) solve2(root, i, ans);
-            else solve1(root, i, ans);
-            if(ans.size()>0){
-            answer.add(ans);
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                TreeNode temp = q.remove();
+                if(temp.left!=null){
+                    q.add(temp.left);
+                }
+                if(temp.right!=null){
+                    q.add(temp.right);
+                }
+
+                if(level%2==0){
+                    ans.addLast(temp.val);
+                }
+                else{
+                    ans.addFirst(temp.val);
+                }
             }
+            level++;
+            answer.add(ans);
+
+
         }
         return answer;
         
